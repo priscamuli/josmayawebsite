6c3224b70b6fb53eee89b26a618ea0747e81b45e
priscamuli <priscakamanthe31@gmail.com>
2025-11-25 17:31:58 +0300

    ready for deployment

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -122,9 +122,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT =os.path.join(BASE_DIR,'statcfiles')
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 
 
 LOGIN_REDIRECT_URL = 'home'
