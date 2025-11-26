b20f0cf83b25deb6dda0ec76ca82ffeb2d096a7c
priscamuli <priscakamanthe31@gmail.com>
2025-11-26 16:28:20 +0300

    corrected cloudinary error

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -94,8 +94,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# MEDIA / FILE STORAGE
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
